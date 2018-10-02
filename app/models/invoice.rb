f256de9e9d76df140bdb0113502aef800324ca51
Tim Fielder <fiel1139@gmail.com>
2018-10-02 13:55:00 -0600

    creates tests for model relationships and validations.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,8 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :customer_id
+  validates_presence_of :merchant_id
+  validates_presence_of :status
+
+  belongs_to :customer
+  belongs_to :merchant
 end
