713cf3cacbac86cbef73da6e33884ad4c6c5e3fc
Tim Fielder <fiel1139@gmail.com>
2018-10-02 14:56:17 -0600

    adding to merchants controller create action.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, except: [:new, :edit]
+      resources :items, except: [:new, :edit]
+      resources :customers, except: [:new, :edit]
+      resources :invoices, except: [:new, :edit]
+      resources :invoice_items, except: [:new, :edit]
+      resources :transactions, except: [:new, :edit]
     end
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,5 +7,30 @@ describe 'Merchants API' do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants.count).to eq(3)
+  end
+  it "can get one merchant by id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+    expect(merchant["id"]).to eq(id)
+  end
+  it "can create a new merchant" do
+    merchant_params = { name: "Sue" }
+
+    post "/api/v1/merchants", params: {merchant: merchant_params}
+
+    merchant = Merchant.last
+
+    assert_response :success
+    expect(response).to be_successful
+    expect(merchant.name).to eq(merchant_params[:name])
   end
 end
