f2cef510c8092354c6ad52dfda1d59b5b1d64e16
Tim Fielder <fiel1139@gmail.com>
2018-10-02 14:24:02 -0600

    revise factory bot.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyString" }
+    name { "Some Call Me . . ." }
+    description { "Monty" }
     unit_price { 1 }
     merchant_id { 1 }
   end
