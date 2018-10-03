229a185a71f94850e647ac7ac5a68884ba5ba481
Tim Fielder <fiel1139@gmail.com>
2018-10-03 16:11:51 -0600

    adds everything for passing merchants endpoint test.

--- app/controllers/api/v1/merchants/merchant_search_controller.rb
+++ app/controllers/api/v1/merchants/merchant_search_controller.rb
@@ -12,11 +12,11 @@ class Api::V1::Merchants::MerchantSearchController < ApplicationController
   end
 
   def show
-    if params[:name] #&& Merchant.find_by_name(params[:name])
+    if params[:name]
       render json: Merchant.find_by_name(params[:name])
-    elsif params[:created_at] #&& Merchant.find_by_created_at(params[:created_at])
+    elsif params[:created_at]
       render json: Merchant.find_by_created_at(params[:created_at])
-    elsif params[:updated_at] #&& Merchant.find_by_updated_at(params[:updated_at])
+    elsif params[:updated_at]
       render json: Merchant.find_by_updated_at(params[:updated_at])
     elsif params[:id]
       render json: Merchant.find(params[:id])
