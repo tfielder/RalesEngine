e9ed17873e666c166f36acb3fb2535de83463ca5
Tim Fielder <fiel1139@gmail.com>
2018-10-03 21:16:54 -0600

    finishes up customer endpoints.

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,6 +1,3 @@
 class CustomerSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name
-
-  has_many :invoices
-  has_many :transactions, through: :invoices
 end
--- db/migrate/20181004030045_add_transactions_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddCustomerToTransaction < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :transactions, :customer, foreign_key: true
-  end
-end
