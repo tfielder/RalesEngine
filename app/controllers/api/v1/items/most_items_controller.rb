44d20c278bb6abae3565764fa1859cc0c10aee5d
Tim Fielder <fiel1139@gmail.com>
2018-10-08 08:47:53 -0600

    removes binding.pry

--- app/controllers/api/v1/items/most_items_controller.rb
+++ app/controllers/api/v1/items/most_items_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Items::MostItemsController < ApplicationController
   def index
-    binding.pry
-    Item.top_items_ranked(params[:quantity])
+    InvoiceItem.top_items_ranked(params[:quantity])
   end
 end
